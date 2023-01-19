2e88e0b00fc93f0a561a9d30d2a1c45983c963ce
h u n <epqmfzkdlwj@naver.com>
2023-01-19 22:06:30 +0900

    Selenium - exam3

---
+++ selenium_exam3.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('C:workspace/chromedriver')
+driver.implicitly_wait(3)
+driver.get("https://www.starbucks.co.kr/store/store_map.do?disp=quick")
+
+target = driver.find_element_by_class_name("quickResultLstCon")
+
+print(type(target))
+print(type(target.text))
+print(target.text)
+driver.quit()
