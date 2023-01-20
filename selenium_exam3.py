386a0b3b074242bfe07fed6988462b2cfe0f9df9
h u n <epqmfzkdlwj@naver.com>
2023-01-21 01:26:55 +0900

    Selenium - exam3 // no more use find_element_by_class_name

--- selenium_exam3.py
+++ selenium_exam3.py
@@ -5,7 +5,8 @@ driver = webdriver.Chrome('C:workspace/chromedriver')
 driver.implicitly_wait(3)
 driver.get("https://www.starbucks.co.kr/store/store_map.do?disp=quick")
 
-target = driver.find_element_by_class_name("quickResultLstCon")
+#target = driver.find_element_by_class_name("quickResultLstCon") no more find_element_by_class_name
+target = driver.find_element(By.CLASS_NAME,"quickResultLstCon")
 
 print(type(target))
 print(type(target.text))
